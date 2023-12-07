ce1586054ba6372d95e273f4a4d6d11a8f6fc0b7
bnthurre <bushrahurre959@gmail.com>
2023-12-07 11:13:58 +0300

    lifting the state up

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const  StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
   @override
   Widget build(context) {
     return Center(
@@ -25,7 +27,7 @@ class StartScreen extends StatelessWidget {
           height: 30,
         ),
         OutlinedButton.icon(
-            onPressed: () {},
+            onPressed:  startQuiz,
             style: OutlinedButton.styleFrom(
                 foregroundColor: const Color.fromARGB(255, 240, 240, 240)),
             icon: const Icon(Icons.arrow_right_alt),
